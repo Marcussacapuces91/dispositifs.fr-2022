2ec720134f9fbb6be7a2900740205ade6826eef8
Marc Sibert <marc@sibert.fr>
2022-06-28 14:18:31 +0200

    Update PicoForm2Mail.php

--- plugins/PicoForm2Mail.php
+++ plugins/PicoForm2Mail.php
@@ -5,6 +5,13 @@ class PicoForm2Mail extends AbstractPicoPlugin
 	function onRequestUrl($url) {
 		if (isset($_POST['send_email'])) {
 			$name = $_POST['name'];
+			if ($name == "CrytoCetCet") {
+				header("HTTP/1.1 404 Va baiser ta mère !");
+				return;
+			}
+
+			$dump = json_encode($_SERVER, JSON_PRETTY_PRINT);
+
 			$email = $_POST['email'];
 			$subject = $_POST['subject'];
 			$message = $_POST['message'];                                                   
@@ -13,7 +20,7 @@ class PicoForm2Mail extends AbstractPicoPlugin
 			$password = "c=A3C=E1=E8=93_=A9=7F=CE=00N]=8E=B6=BB";
 			$to = openssl_decrypt(quoted_printable_decode($encode), 'aes128', quoted_printable_decode($password));
 			$email_subject = 'Fiche contact de dispositifs.fr';
-			$body = "Nom: $name\nEmail: $email\nSujet: $subject\n\nMessage ci-dessous:\n$message";
+			$body = "Nom: $name\nEmail: $email\nSujet: $subject\n\nMessage ci-dessous:\n$message\n---\nSERVER:\n$dump";
 			$from = 'From: contact@dispositifs.fr';
 
 			mail($to, $email_subject, $body, $from) or die("Erreur de transmission du message");
