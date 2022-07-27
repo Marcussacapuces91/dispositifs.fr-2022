12451b0f27a2112684b664fa05b59238af7449e9
Marc Sibert <Marcussacapuces91@users.noreply.github.com>
2022-07-27 16:55:35 +0200

    Update PicoForm2Mail.php
    Intégration de hCaptcha

--- plugins/PicoForm2Mail.php
+++ plugins/PicoForm2Mail.php
@@ -1,16 +1,22 @@
 <?php
 
+
 class PicoForm2Mail extends AbstractPicoPlugin
 {
 	function onRequestUrl($url) {
 		if (isset($_POST['send_email'])) {
-			$name = $_POST['name'];
-			if ($name == "CrytoCetCet") {
-				header("HTTP/1.1 404 Va baiser ta mère !");
+
+			include($_SERVER['DOCUMENT_ROOT']."/secret.php");
+
+			$token = $_POST['h-captcha-response'];
+			$remote = $_SERVER['REMOTE_ADDR'];
+			$response = file_get_contents("https://hcaptcha.com/siteverify?sitekey=$site_key&secret=$secret&response=$token&remoteip=$remote");
+			if (!json_decode($response)->success) {
+				header("HTTP/1.1 404 Captcha error!");
 				return;
 			}
 
-			$dump = json_encode($_SERVER, JSON_PRETTY_PRINT);
+			$dump = json_encode($_SERVER, JSON_PRETTY_PRINT) . json_encode($_POST, JSON_PRETTY_PRINT) . $response;
 
 			$email = $_POST['email'];
 			$subject = $_POST['subject'];
@@ -20,7 +26,7 @@ class PicoForm2Mail extends AbstractPicoPlugin
 			$password = "c=A3C=E1=E8=93_=A9=7F=CE=00N]=8E=B6=BB";
 			$to = openssl_decrypt(quoted_printable_decode($encode), 'aes128', quoted_printable_decode($password));
 			$email_subject = 'Fiche contact de dispositifs.fr';
-			$body = "Nom: $name\nEmail: $email\nSujet: $subject\n\nMessage ci-dessous:\n$message\n---\nSERVER:\n$dump";
+			$body = "Nom: $name\nEmail: $email\nSujet: $subject\n\nMessage ci-dessous:\n$message\n---\nSERVER & POST:\n$dump";
 			$from = 'From: contact@dispositifs.fr';
 
 			mail($to, $email_subject, $body, $from) or die("Erreur de transmission du message");
